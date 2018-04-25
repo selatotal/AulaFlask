d4c28c99237cc37d79aa1892dc8285aee97408c0
Tales Bitelo Viegas <selatotal@gmail.com>
2018-04-25 20:22:02 -0300

    Flask Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import config
+from flask_bootstrap import Bootstrap
+bootstrap = Bootstrap
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -7,6 +9,9 @@ def create_app(config_name):
 
     from .talks import talks as talks_blueprint
     app.register_blueprint(talks_blueprint)
+
+    # Inicializando app Bootstrap
+    bootstrap(app)
 
     return app
 
