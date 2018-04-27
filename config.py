95b409dfadc6de96c45e8c0c3b9994a556ff221e
Tales Bitelo Viegas <selatotal@gmail.com>
2018-04-27 17:44:40 -0300

    Registro de usuários

--- config.py
+++ config.py
@@ -10,15 +10,19 @@ class DevelopmentConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SeGr&d0'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
--- manage.py
+++ manage.py
@@ -2,9 +2,26 @@
 import os
 from app import create_app
 from flask_script import Manager
+from app import db
+from app.models import User
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+
+@manager.command
+def adduser(email, username, admin=False):
+    """ Registra um novo usuário """
+    from getpass import getpass
+    password = getpass()
+    password2 = getpass(prompt="Confirme: ")
+    if password != password2:
+        import sys
+        sys.exit('Erro: senhas nao conferem')
+    db.create_all()
+    user = User(email=email, username=username, password=password, is_admin=admin)
+    db.session.add(user)
+    db.session.commit()
+    print('Usuario {0} foi registrado com sucesso.'.format(username) )
 
 if __name__ == '__main__':
     manager.run()
